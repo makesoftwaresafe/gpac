makesoftwaresafe/gpac

commit b325a93c44bed8d95aa0f9cc042f5c4f470a6338

fuzz: fix utf stack overflow + null deref

src/filters/load_text.c
@@ -429,6 +429,8 @@ char *gf_text_get_utf8_line(char szLine[2048], u32 lineSize, FILE *txt_in, s32 u
 			i+=2;
 		}
 	}
+	szLine[2046]=0;
+	szLine[2047]=0;
 	sptr = (u16 *)szLine;
 	i = gf_utf8_wcstombs(szLineConv, 2048, (const unsigned short **) &sptr);
 	if (i == GF_UTF8_FAIL) i = 0;

src/filters/in_rtp_stream.c
@@ -345,6 +345,7 @@ GF_RTPInStream *rtpin_stream_new(GF_RTPIn *rtp, GF_SDPMedia *media, GF_SDPInfo *
 	mid = prev_stream = base_stream = 0;
 	i=0;
 	while ((att = (GF_X_Attribute*)gf_list_enum(media->Attributes, &i))) {
+		if (!att->Value) continue;
 		if (!stricmp(att->Name, "control")) ctrl = att->Value;
 		else if (!stricmp(att->Name, "gpac-broadcast")) force_bcast = GF_TRUE;
 		else if (!stricmp(att->Name, "mpeg4-esid") && att->Value) ESID = atoi(att->Value);

commit bacad447d273aa68fba363981558eb489091c352

fixed bug introduced in b325a93c

src/filters/load_text.c
@@ -331,6 +331,7 @@ char *gf_text_get_utf8_line(char szLine[2048], u32 lineSize, FILE *txt_in, s32 u
 	unsigned short *sptr;
 	Bool in_eof = *io_progress;
 	*io_progress = GF_FALSE;
+	if (lineSize<2) return NULL;
 
 	memset(szLine, 0, sizeof(char)*lineSize);
 	sOK = gf_fgets(szLine, lineSize, txt_in);
@@ -429,8 +430,8 @@ char *gf_text_get_utf8_line(char szLine[2048], u32 lineSize, FILE *txt_in, s32 u
 			i+=2;
 		}
 	}
-	szLine[2046]=0;
-	szLine[2047]=0;
+	szLine[lineSize-2]=0;
+	szLine[lineSize-1]=0;
 	sptr = (u16 *)szLine;
 	i = gf_utf8_wcstombs(szLineConv, 2048, (const unsigned short **) &sptr);
 	if (i == GF_UTF8_FAIL) i = 0;